cae70f1ed1be8650747db7eb3c3aa9fc91132471
arkmadj <ahmadjinadu@gmail.com>
2022-01-31 14:41:02 +0100

    first commit

---
+++ test-components-vue/dev/serve.vue
@@ -0,0 +1,21 @@
+<script>
+import { defineComponent } from 'vue';
+import AppBadge from '../src/lib-components/app-badge/AppBadge.vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { TestComponentsVueSample } from '@/entry.esm';
+
+export default defineComponent({
+	components: { AppBadge },
+  name: 'ServeDev',
+  // components: {
+  //  TestComponentsVueSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <!-- <test-components-vue-sample /> -->
+    <app-badge label="Approved"/>
+  </div>
+</template>
---
+++ test-components-vue/src/lib-components/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+export { default as TestComponentsVueSample } from './test-components-vue-sample.vue';
+export {default as AppBadge} from './app-badge/AppBadge.vue';
