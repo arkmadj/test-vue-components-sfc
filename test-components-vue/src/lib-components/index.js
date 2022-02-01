bfccee35fd42e1e75801b13f152c096bea3d01ce
arkmadj <ahmadjinadu@gmail.com>
2022-02-01 15:05:06 +0100

    before I add quasar

--- test-components-vue/src/lib-components/index.js
+++ test-components-vue/src/lib-components/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-export { default as TestComponentsVueSample } from './test-components-vue-sample.vue';
+// export { default as TestComponentsVueSample } from './test-components-vue-sample.vue';
 export {default as AppBadge} from './app-badge/AppBadge.vue';
 export {default as AppLineChart} from './app-line-chart/AppLineChart';
 export {default as GraphCard} from './app-graph-card/GraphCard';
